06d0fa878c976a3da541275f06ef53c91551d2ab
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-12 22:12:57 +0100

    Updates

---
+++ precache-manifest.8a3f95e57f6176267557b332554cc57d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39cff1a79defb0fe09c",
+    "url": "/todolist-react/static/js/runtime~main.d39cff1a.js"
+  },
+  {
+    "revision": "47000ec3dbba3e098380",
+    "url": "/todolist-react/static/js/main.47000ec3.chunk.js"
+  },
+  {
+    "revision": "90ad2b26e6ecd4fcb1c0",
+    "url": "/todolist-react/static/js/1.90ad2b26.chunk.js"
+  },
+  {
+    "revision": "47000ec3dbba3e098380",
+    "url": "/todolist-react/static/css/main.aedf54b4.chunk.css"
+  },
+  {
+    "revision": "1faddd99a91de61a65118b58f0d9a350",
+    "url": "/todolist-react/index.html"
+  }
+];
